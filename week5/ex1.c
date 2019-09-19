0658307859f4bf22382ebfa8c152ff7ca820b258
nmaria03 <42997877+nmaria03@users.noreply.github.com>
2019-09-19 23:26:14 +0300

    Add files via upload

--- week5/ex1.c
+++ week5/ex1.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
-void *fun(int n) {
+
+void *func(int n) {
     printf("%d\n", n + 1);
     pthread_exit(NULL);
 }
+
 int main() {
     pthread_t th[10];
-    for(int i = 0; i < 10; i++) {
-        pthread_create(&th[i], NULL, fun, (void*)i);
+    for (int i = 0; i < 10; i++) {
+        pthread_create(&th[i], NULL, func, (void*)i);
         printf("%s %d\n", "Thread number ", i + 1);
         pthread_join(th[i], NULL);
     }
